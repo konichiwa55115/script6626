3e913ef2f10c278080947936d7806757a925dc8d
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-26 02:17:12 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,30 +1,5 @@
 
 
-wget -O entry https://new2.edithxbase.eu.org/90707/free-6234365091
-python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
-uploadgram -1001821573758 transcription.txt
-
-
-
-wget -O entry https://new2.edithxbase.eu.org/90709/free-6234365091
-python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
-uploadgram -1001821573758 transcription.txt
-
-
-
-
-wget -O entry https://new2.edithxbase.eu.org/90711/free-6234365091
-python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
-uploadgram -1001821573758 transcription.txt
-
-
-
-wget -O entry https://new2.edithxbase.eu.org/90713/free-6234365091
-python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
-uploadgram -1001821573758 transcription.txt
-
-
-
 
 wget -O entry https://new2.edithxbase.eu.org/90715/free-6234365091
 python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
