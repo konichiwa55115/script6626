d5c9157aa9689d84bc26bcbb44610f891b0b1b7f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-26 02:22:16 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,68 @@
 
 
 
-wget -O entry https://new2.edithxbase.eu.org/90715/free-6234365091
+wget -O entry https://new2.edithxbase.eu.org/90811/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90810/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90817/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90816/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90814/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90821/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90820/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90822/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90830/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90826/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90827/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90831/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90832/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90833/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+wget -O entry https://new2.edithxbase.eu.org/90838/free-6234365091
 python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
 uploadgram -1001821573758 transcription.txt
 
 
+wget -O entry https://new2.edithxbase.eu.org/90844/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
 
