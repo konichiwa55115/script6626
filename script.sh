12e3583cdf9a6613133d73adf1b9a4dc469792ac
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-10-06 19:32:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,36 +1,48 @@
 
-wget -O pic https://new2.edithxbase.eu.org/149430/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149442/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151077/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151118/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149426/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149443/free-6234365091
+
+
+wget -O pic https://new2.edithxbase.eu.org/151078/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151108/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149431/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149448/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151081/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151112/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149427/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149444/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151083/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151114/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149434/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149450/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151084/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151115/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149435/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149451/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151087/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151109/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
-wget -O pic https://new2.edithxbase.eu.org/149438/free-6234365091
-wget -O aud https://new2.edithxbase.eu.org/149454/free-6234365091
+wget -O pic https://new2.edithxbase.eu.org/151088/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151119/free-6234365091
+ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
+uploadgram -1001821573758 mp4file.mp4
+
+wget -O pic https://new2.edithxbase.eu.org/151090/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151121/free-6234365091
+ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
+uploadgram -1001821573758 mp4file.mp4
+
+wget -O pic https://new2.edithxbase.eu.org/151153/free-6234365091
+wget -O aud https://new2.edithxbase.eu.org/151181/free-6234365091
 ffmpeg -r 1 -loop 1 -y -i  pic -i aud -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 mp4file.mp4
 uploadgram -1001821573758 mp4file.mp4
 
